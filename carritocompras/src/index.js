b47044750b203e20e210ae76c630b7ac31ae7f3d
Leandro <80827721+LeandroGarza@users.noreply.github.com>
2022-06-07 15:03:04 -0300

    mejora de interfaz de login

--- carritocompras/src/index.js
+++ carritocompras/src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import Routes from './routes/Routes';
-//import reportWebVitals from './reportWebVitals';
-
-/*const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>
-);*/
 
 ReactDOM.render(
   <React.StrictMode>
--- carritocompras/src/pages/Login/Login.js
+++ carritocompras/src/pages/Login/Login.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import md5 from 'md5';
 import Cookies from 'universal-cookie'
+import bici2 from '../../images/bici.jpg';
 
 const baseUrl="http://localhost:3001/usuarios"    //url de nuestra api!
 const cookies = new Cookies();
@@ -58,146 +59,32 @@ class Login extends Component{
 
     render(){
         return(
-    <div className="containerPrincipal">
-        <div className="containerSecundario">
-            <div className="form-group">
-                <label>Usuario: </label>
-                <br />
-                <input
-                    type="text"
-                    className="form-control"
-                    name="username"
-                    onChange={this.handleChange}
-                />
-                <br />
-                <label>Contraseña</label>
-                <br />
-                <input
-                    type= "password"
-                    className="form-control"
-                    name="password"
-                    onChange={this.handleChange}
-                />
-                <br />
-                <button className='btn btn-primary' onClick={()=> this.iniciarSesion()}> Iniciar Sesion</button>
+            <div className="App">
+                <header className="App-header">
+                    <h1>BiciMundo "Tres F"</h1>
+                    <h3><i><u>Bicicletas de Calidad Premium</u></i></h3>
+                    <img src={bici2}  className="App-logo" alt="logo"/>
+
+                    <br />
+                    <input
+                        type="text"
+                        placeholder='Usuario'
+                        name="username"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input
+                        type= "password"
+                        placeholder='Contraseña'
+                        name="password"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <button className='btn btn-primary' onClick={()=> this.iniciarSesion()}> Iniciar Sesion</button>
+                </header>
             </div>
-        </div>    
-    </div>
         );
     }
 }
-
-
-/*import React, {useState} from 'react';
-import './Login.css';
-import Title from './Components/Title/Title';
-import Label from './Components/label/label';
-import Input from './Components/input/input';
-
-const Login = () => {
-
-    const [ user , setUser ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ passwordError , setPasswordError ] = useState(false);
-    const [ isLogin , setIsLogin ] = useState(false);
-    const [ hasError , setHasError ] = useState(false);
-
-    function handleChange(name, value){
-        if(name == 'usuario'){
-            setUser(value)
-            setHasError(false);
-        }else{
-            if(value < 4){
-                setPasswordError(true);
-                setHasError(false);
-            }else{
-                setPasswordError(false);
-                setPassword(value);
-                setHasError(false);
-            }
-        }
-    };
-
-    function ifMatch(param){
-        if(param.user.length > 0 && param.password.length > 0){
-            if(param.user === 'Leandro' && param.password === '12345'){
-                const { user , password } = param;
-                let ac = { user , password };
-                let account = JSON.stringify(ac);
-                localStorage.setItem('account', account);
-                setIsLogin(true);
-            }else{
-                setIsLogin(false);
-                setHasError(true);
-            }
-        }else{
-            setIsLogin(false);
-            setHasError(true);
-        }
-    }
-
-    function handleSubmit(){
-        let account = { user , password}
-        if(account){
-            console.log('account:' , account)
-        }
-    }
-
-    return (
-        <div className='login-container'>
-        { isLogin ?
-            <div className='home-container'>
-                <h1>hola, {user}</h1>
-                <label>estas logueado</label>
-            </div>
-        :
-            <div className='login-content'>
-                <title text = 'Bienvenido' />
-            
-                {hasError &&
-                    <label className='label-alert'>
-                      usuario o contrasena incorrecta
-                    </label>
-                }
-
-                <label text='Usuario' />
-                <input
-                attribute={{
-                 id: 'usuario',
-                 name: 'usuario',
-                 type: 'text',
-                 placeholder: 'Ingrese su usuario'
-                }}
-                handleChange={handleChange}
-                />
-
-                <label text = 'Contraasenia' />
-                 <input
-                attribute={{
-                  id: 'Contraseña',
-                  name: 'Contraseña',
-                  type: 'password',
-                 placeholder: 'Ingrese su Contraseña'
-             }}
-             handleChange={handleChange}
-             param={passwordError}
-                />
-
-                { passwordError &&
-                    <label className='label-error'>
-                       Contraseña invalida
-                    </label>
-                }
-
-                <div className='submit-button-container'>
-                    <button onClick={handleSubmit} classname = 'submit'>
-                        Ingresar
-                    </button>
-                </div>
-            </div>
-        }
-        </div> 
-    )
-};*/
 
 export default Login;
